da3204a5116726a594cf2c0427679fffeb58d56a
youssef <youssefkhaled2019@gmail.com>
2024-01-13 19:33:28 +0200

    day_2

--- tickets_api/urls.py
+++ tickets_api/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home), 
-     #1  http://127.0.0.1:8000/no_rest_no_model
+    #1  http://127.0.0.1:8000/no_rest_no_model
     path("no_rest_no_model",views.no_rest_no_model),
-     #2  http://127.0.0.1:8000/no_rest_from_model
+    #2  http://127.0.0.1:8000/no_rest_from_model
     path("no_rest_from_model",views.no_rest_from_model),
     #3.1 GET POST from rest framwork function based view @api_view
     #http://127.0.0.1:8000/fbv_user
@@ -13,4 +13,16 @@ urlpatterns = [
     #3.2 GEY PUT DELETE from rest framwork function based view @api_view
     #http://127.0.0.1:8000/fbv_user_id/1
     path("fbv_user_id/<int:id>",views.fbv_user_id),
+    #4.1  GET POST  from rest framwork class based view APIView
+    #http://127.0.0.1:8000/rest_cbv/
+    path("rest_cbv/",views.CBV_List.as_view()),
+    #4.2 GEY PUT DELETE from rest framwork class based view APIView
+    #http://127.0.0.1:8000/rest_cbv/1
+    path("rest_cbv/<int:pK>",views.CBV_pk.as_view()),
+    #5.1  GET POST  from rest framwork class based view Mixins
+    #http://127.0.0.1:8000/rest_mixins/
+    path("rest_mixins/",views.mixins_list.as_view()),
+    #5.2  GEY PUT DELETE from rest framwork class based view Mixins
+     #http://127.0.0.1:8000/rest_mixins/1
+    path("rest_mixins/<int:pk>",views.mixins_pk.as_view()),
 ]
--- tickets_api/views.py
+++ tickets_api/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse,JsonResponse 
+from django.http import HttpResponse,JsonResponse ,Http404
 from .models import Move,User,Reservation
 from  rest_framework.decorators import api_view
 from .serializers import MoveSerializers ,UserSerializers,ReservationSerializers
 from  rest_framework.response import Response
-from  rest_framework import status ,filters
+from  rest_framework import status ,filters ,generics,mixins
+from rest_framework.views import APIView
 # Create your views here.
 def home(request):
     return HttpResponse("ddddd")
@@ -79,3 +80,74 @@ def fbv_user_id(request,id):
         data.delete()
         response={"message":"delete data "}
         return Response(response,status=status.HTTP_204_NO_CONTENT)
+    
+
+#CBV class based views
+#4.1 list and create  -> GET POST
+class CBV_List(APIView):
+    def get(self,request):#two pramter
+         guests=User.objects.all()
+         serializer=UserSerializers(guests,many=True) #x serializer
+         return Response(serializer.data)
+    def post(self,request):
+        serializer=UserSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+
+
+
+#4.2 GET PUT DELETE cloass based views  -> pk
+class CBV_pk(APIView):
+    
+    
+    def get_object(self,pK): #x  get_object
+          try:
+             return User.objects.get(id=pK)
+          except User.DoesNotExist :
+            raise Http404
+
+       
+    def get(self,request,pK):
+        
+        guest=self.get_object(pK) 
+        serializer= UserSerializers(guest)
+        return Response(serializer.data)
+
+    def put(self,request,pK):
+        guest=self.get_object(pK)
+        serializer= UserSerializers(guest,data=request.data) 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)#,status=status.HTTP_201_CREATED
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
+    
+    def delete(self,request,pK):
+           guest=self.get_object(pK)
+           guest.delete()
+           return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+#5 Mixins don't repeat yourself
+#5.1 mixins list
+class mixins_list(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset =User.objects.all() #X queryset
+    serializer_class=UserSerializers  #X serializer_class
+    def get(self,request):
+        return self.list(request)
+    def post(self,request):
+        return self.create(request)
+
+
+#5.2 mixins GET PUT DELETE      
+class mixins_pk(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin,generics.GenericAPIView):
+    queryset =User.objects.all()
+    serializer_class=UserSerializers
+                                          
+    def get(self,request,pk):
+        return self.retrieve(request)  #RetrieveModelMixin for return  one element
+    def put(self,request,pk):
+        return self.update(request) 
+    def delete(self,request,pk):
+        return self.destroy(request)    
